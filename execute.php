c42c43f067cece093d1eace8f4f6b03c6215efde
shopcarbon12-gif <YOUR_GITHUB_EMAIL>
2026-03-03 17:18:28 -0500

    chore: remove accidental temp files from root

--- execute.php
+++
@@ -1,6 +0,0 @@
-<?php
-$app = \App\Models\Application::where('uuid', 'aw4800s4wsgok0wck480goco')->first();
-$deployment_uuid = new \Visus\Cuid2\Cuid2;
-$result = queue_application_deployment(application: $app, deployment_uuid: $deployment_uuid, force_rebuild: true, pull_request_id: 0, is_api: true);
-echo json_encode($result) . PHP_EOL;
-
